56dd591f2525a8801225d95dd98b1f3dc31e3666
Oliver Zhang <oli.zhang@gmail.com>
2018-10-16 07:15:38 +0800

    more duration choices

--- app/scripts/page-maker.js
+++ app/scripts/page-maker.js
@@ -36,7 +36,7 @@
         'sponsorMobile': ['no', 'yes'],
         'inSponsor': ['no', 'yes'],
         'hideDownloadButton': ['no', 'yes'],
-        'durationInSeconds': ['default','15','30','60','90'],
+        'durationInSeconds': ['default','5','6','7','8','9','10','15','30','60','90'],
         'weight': ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'],
         'priority': ['', 'sponsorship', 'standard', 'house'],
         'closeButton': ['none','LeftTop','RightTop','LeftBottom','RightBottom'],
