2f713145e2d75a0a778f963781d3a87e8e3511f9
Oliver Zhang <oli.zhang@gmail.com>
2020-12-16 12:05:15 +0800

    update

--- app/scripts/page-maker.js
+++ app/scripts/page-maker.js
@@ -340,7 +340,6 @@
             'feedType',
             'feedImage',
             'ShowBodyMail',
-            'ShowParagraphs',
             'moreLink'
           ],
           listWithText: [
@@ -1260,9 +1259,8 @@
           'yes',
           'no'
         ],
-        ShowBodyMail: ['no', 'yes'],
-        ShowParagraphs: 'number'
-      };
+        ShowBodyMail: ['no', 'yes']
+    };
 
 
     var dataRulesTitle = {
@@ -1318,8 +1316,7 @@
         PendingOffer: 'iOS用户已经redeem了Subscription Offer，但是还没有付款',
         InfoCollection: 'basic表示只收集邮件和手机号码，detail表示还要收集更多信息, china_only：验证的时候只接受中国大陆的手机号码，13位数字，如果地址填写的不是中国大陆，允许提交，但是会弹出反馈。',
         discountCode: '目前的约定如下：ft_discount为85折, ft_renewal为75折, ft_win_back为5折',
-        ShowBodyMail: '在邮件中显示文章全文',
-        ShowParagraphs: '邮件中显示文章的段落'
+        ShowBodyMail: '在邮件中显示文章全文'
       };
 
     // MARK: - Differentiate subscription information
