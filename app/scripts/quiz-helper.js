d33efba8e8fd6530ff4d0f9acc65acc3df67727e
Oliver Zhang <oli.zhang@gmail.comm>
2021-11-28 20:22:17 +0800

    update

--- app/scripts/quiz-helper.js
+++ app/scripts/quiz-helper.js
@@ -1,6 +1,7 @@
  /*exported expand, copyAlert */
 var itemDict = {};
 var currentId;
+var isNewsQuiz = false;
 function loadContent() {
     var api = '/falcon.php/jsapi/get_info_for_quiz';
     if (window.location.hostname === 'localhost') {
@@ -24,6 +25,25 @@ function render(items) {
         html += '<div class="item-container" id="' + id + '" ><div class="headline" onclick="expand(this)">' + items[i].cheadline + '</div><div class="detail"></div></div>'; 
     }
     document.querySelector('.content').innerHTML = html;
+}
+
+function renderOneItem() {
+    var headline = window.opener.document.getElementById('cheadline').value;
+    var body = window.opener.document.getElementById('cbody').value;
+    const isStory = window.opener && window.opener.location.href.indexOf('story') >= 0;
+    if (isStory === false) {
+        body = `<p>${body.split('\n').map(x=>`<p>${x}</p>`).join('')}</p>`;
+    }
+    document.querySelector('.content').innerHTML = '<div class="item-container"><div class="headline">' + headline + '</div><div class="detail">' + body +'</div></div>'; 
+}
+
+function start() {
+    isNewsQuiz = !(window.opener && ((window.opener.document.getElementById('tag') && window.opener.document.getElementById('tag').value.indexOf('双语阅读') >= 0) || window.opener.location.href.indexOf('story') >= 0));
+    if (isNewsQuiz) {
+        loadContent();
+    } else {
+        renderOneItem();
+    }
 }
 
 function expand(ele) {
@@ -50,23 +70,19 @@ function copyAlert(error) {
     alert('在复制的时候浏览器报错了，请手动复制！\n错误信息可以截屏给开发者：' + error.toString());
 }
 
-document.querySelector('.create-quiz').onclick = function() {
-    try {
-        var selObj = window.getSelection();
-        var text = selObj.toString().replace(/\n|\r/g, '');
-        if (!text || text === '') {return;}
-        var item = itemDict[currentId];
-        var headline = item.cheadline;
-        var link = `/${item.type}/${item.id}`;
-        var image = '';
-        if (item.story_pic) {
-            var src = item.story_pic.smallbutton || item.story_pic.cover || item.story_pic.bigbutton || '';
-            src = src.replace(/\/upload\//g, '/').replace(/\/unsafe\/picture/g, '/unsafe/450x0/picture');
-            if (src !== '') {
-                image = `<div><img src="${src}"></div>`;
-            }
+function getNewsQuiz(text) {
+    var item = itemDict[currentId];
+    var headline = item.cheadline;
+    var link = `/${item.type}/${item.id}`;
+    var image = '';
+    if (item.story_pic) {
+        var src = item.story_pic.smallbutton || item.story_pic.cover || item.story_pic.bigbutton || '';
+        src = src.replace(/\/upload\//g, '/').replace(/\/unsafe\/picture/g, '/unsafe/450x0/picture');
+        if (src !== '') {
+            image = `<div><img src="${src}"></div>`;
         }
-        var oneSnippet = `<span>
+    }
+    var oneSnippet = `<span>
 <div class="pagetitle">单选题</div>
 <div class="pageintro is-text-only" id="id-0">${text}？</div>
 <div>
@@ -85,8 +101,35 @@ ${image}
 
 
 `;
+    return oneSnippet;
+}
+
+function getInStoryQuiz(text) {
+    const oneSnippet = `<span>
+<div class="pageintro">${text}？</div>
+<div><div class="rightanswer">${text}</div>
+<ul class="quizlist">
+<li onclick="quizcheck(this)" value="1"></li>
+<li onclick="quizcheck(this)" value="0"></li>
+<li onclick="quizcheck(this)" value="0"></li>
+</ul>
+</div>
+</span>
+
+
+`;
+    return oneSnippet;
+}
+
+document.querySelector('.create-quiz').onclick = function() {
+    try {
+        var selObj = window.getSelection();
+        var text = selObj.toString().replace(/\n|\r/g, '');
+        if (!text || text === '') {return;}
+        const oneSnippet = isNewsQuiz ? getNewsQuiz(text) : getInStoryQuiz(text);
         document.querySelector('.result').value += oneSnippet;
     } catch(err) {
+        console.log(err);
         alert('Please select the right text first! ');
     }
 };
@@ -94,6 +137,9 @@ ${image}
 
 document.querySelector('.finish').onclick = function(){
     var textarea = document.querySelector('.result');
+    if (isNewsQuiz === false) {
+        textarea.value = `<div class="card-container"><div class="subtitle">快问快答</div><div class="speedread-questions">${textarea.value}</div></div>`.replace(/[\n\r]+/g, '');
+    }
     textarea.select();
     // document.execCommand('copy');
     // if (window.opener) {
@@ -112,9 +158,11 @@ document.querySelector('.finish').onclick = function(){
         })
         .catch((error) => { copyAlert(error); });
     } else {
-        copyAlert('navigator.clipboard不支持。可能是因为网址的https的问题，或者浏览器不支持。');
+        copyAlert('navigator.clipboard不支持。可能是因为网址的https的问题，或者浏览器不支持。请手动复制。');
     }
     /* jshint ignore:end */
 };
 
-loadContent();
+
+
+start();
